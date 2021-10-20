129e5c7f844c3d69789722d130bb9ab6474749fa
Steven-Cadena <steven.cadena.giler@gmail.com>
2021-10-20 11:24:29 +0200

    comit

--- src/components/Contador/contador.js
+++ src/components/Contador/contador.js
@@ -1,6 +1,14 @@
 //DEBEMOS IMPORTAR SIEMPRE {Component} CON JSX
 import {Component} from 'react';
 
+//TAMBIEN PODEMOS UTILIZAR function con 
+//SINTAXIS DE JavaScript PARA SEPARAR CODIGO 
+//LOGICAMENTE, EN ESTA FUNCION NO PODEMOS 
+//UTILIZAR NADA DE LA CLASE
+function miFuncion(){
+    console.log("Soy una función fuera de la clase");
+}
+
 class Contador extends Component{
 
     render(){
@@ -17,6 +25,7 @@ class Contador extends Component{
             <button onClick={()=>{
                 // SI EL METODO ESTÁ EN RENDER LA LLAMADA SE REALIZA COMO SIEMPRE
                 miMetodoRender();
+                miFuncion();
             }}>
                 Pulsa y haz algo
             </button>

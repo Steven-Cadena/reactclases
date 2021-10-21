7a4cb79cd6b46bf5e94fdc66aad0987cb8618014
Steven-Cadena <steven.cadena.giler@gmail.com>
2021-10-21 10:43:13 +0200

    commit

--- src/components/Deportes/Deportes.js
+++ src/components/Deportes/Deportes.js
@@ -3,17 +3,48 @@ import Deporte from "./Deporte";
 
 //COMPONENTE PADRE
 class Deportes extends Component{
+    //DEBEMOS CREA UN DEPORTE FAVORITO EN STATE 
+    //PARA PODER CAMBIARLO AL SELECCIONAR
+    state = {
+        seleccionado: "",
+        //AQUI TENDREMOS UN list  CON TODOS LOS DEPORTES Y 
+        //SERA DINAMICOS(pintamos dentro del state) 
+        deportes : ["Padel","Petanca","Curling","Futbol","Tenis","Canicas"]
+    };
+    //TENEMOS EL METODO DEL PARENT  
+    mostrarFavorito = (deporteFavorito) =>{
+        //TENGO QUE RECIBIR ALGO DEL HIJO ...parametro
+        //console.log("Favorito " + deporteFavorito);
+        this.setState({
+            seleccionado: deporteFavorito,
+        });
+    };
 
+    insertarDeporte = () => {
+        this.state.deportes.push("Poker");
+        this.setState({
+            deportes: this.state.deportes
+        });
+    };
     //AQUI TENDREMOS UN list  CON TODOS LOS DEPORTES Y 
     //SERA ESTATICO (NO VA AL STATE) 
-    deportes = ["Padel","Petanca","Curling","Futbol","Tenis","Canicas"];
+    //deportes = ["Padel","Petanca","Curling","Futbol","Tenis","Canicas"];
 
     render(){
         return(
             <div>
+                <h2>
+                    Su deporte favorito es:
+                    <span style={{color:"red"}}> 
+                        {this.state.seleccionado}
+                    </span>
+                </h2>
+                <button onClick= { (this.insertarDeporte) }>
+                    Nuevo Deporte
+                </button>
                 <ul>
-                    {this.deportes.map((depor,index)=>{
-                        return (<Deporte nombre={depor} key={index}/>)
+                    {this.state.deportes.map((depor,index)=>{
+                        return (<Deporte nombre={depor} key={index} mostrarFavorito={this.mostrarFavorito}/>)
                     })}
                 </ul>
             </div>
